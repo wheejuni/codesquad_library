e3d47deb6f783dceefacf40e0384739af609be56
wheejuni <wheejuni@gmail.com>
2017-08-22 18:00:36 +0900

    Amazon RDS 대응, Elastic Beanstalk 배포용 소스코드 수정

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO user(userid, email, is_admin, loginid, name, profile_path, pwd, slack_id) VALUES (1, 'wheejuni@me.com', 1, 'wheejuni', '정휘준', null, '1234', 'wheejuni'); 
-INSERT INTO user(userid, email, is_admin, loginid, name, profile_path, pwd, slack_id) VALUES (2, 'seulgi@smtown.com', 0, 'seulgi', '강슬기', null, '1234', 'seulgi'); 
-INSERT INTO book(author, publisher, is_possessed, title) VALUES ('이말년', '네이버웹툰', 0, '이말년서유기');
-INSERT INTO book(author, publisher, is_possessed, title) VALUES ('이말년', '피키캐스트', 0, '이말년 수필');
-INSERT INTO book(author, publisher, is_possessed, title) VALUES ('박재성', '코드스쿼드', 0, '자바 이렇게하면 무조건 성공한다');
-
