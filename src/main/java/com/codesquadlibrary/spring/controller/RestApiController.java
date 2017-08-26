ce19bfd006643bab183ff119df81e40287da7e9d
wheejuni <wheejuni@gmail.com>
2017-08-27 00:40:46 +0900

    initiate new branch

--- src/main/java/com/codesquadlibrary/spring/controller/RestApiController.java
+++ src/main/java/com/codesquadlibrary/spring/controller/RestApiController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestApiController {
 	
-	//TODO Implement GET REQUEST handler for Slack Incoming-webhooks integration. 
-	
+
 
 }
